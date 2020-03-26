d702eaf6f10fdcc058bead18a5d7255a6e5fc5c3
cnorm <30735143+cnorm@users.noreply.github.com>
2020-03-26 07:41:05 -0600

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,98 @@
+function requestChatBot() {
+    const oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", initBotConversation);
+    var path = "/chatBot";
+    oReq.open("POST", path);
+    oReq.send();
+}
+
+function initBotConversation() {
+    if (this.status >= 400) {
+        alert(this.statusText);
+        return;
+    }
+    // extract the data from the JWT
+    const jsonWebToken = this.response;
+    const tokenPayload = JSON.parse(atob(jsonWebToken.split('.')[1]));
+    const user = {
+        id: tokenPayload.userId,
+        name: tokenPayload.userName
+    };
+    let domain = undefined;
+    if (tokenPayload.directLineURI) {
+        domain =  "https://" +  tokenPayload.directLineURI + "/v3/directline";
+    }
+    var botConnection = window.WebChat.createDirectLine({
+        token: tokenPayload.connectorToken,
+        domain: domain
+    });
+    const styleOptions = {
+        botAvatarImage: 'https://docs.microsoft.com/en-us/azure/bot-service/v4sdk/media/logo_bot.svg?view=azure-bot-service-4.0',
+        // botAvatarInitials: '',
+        // userAvatarImage: '',
+        hideSendBox: false, /* set to true to hide the send box from the view */
+        botAvatarInitials: 'Bot',
+        userAvatarInitials: 'You',
+        backgroundColor: '#F8F8F8'
+    };
+
+    const store = window.WebChat.createStore(
+        {},
+        function(store) {
+            return function(next) {
+                return function(action) {
+                    if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
+
+                        // Use the following activity to enable an authenticated end user experience
+                        /*
+                        store.dispatch({
+                            type: 'WEB_CHAT/SEND_EVENT',
+                            payload: {
+                                name: "InitAuthenticatedConversation",
+                                value: jsonWebToken
+                            }
+                        });
+                        */
+
+                        // Use the following activity to proactively invoke a bot scenario
+                        /*
+                        store.dispatch({
+                            type: 'DIRECT_LINE/POST_ACTIVITY',
+                            meta: {method: 'keyboard'},
+                            payload: {
+                                activity: {
+                                    type: "invoke",
+                                    name: "TriggerScenario",
+                                    value: {
+                                        trigger: "{scenario_id}",
+                                        args: {
+                                            myVar1: "{custom_arg_1}",
+                                            myVar2: "{custom_arg_2}"
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                        */
+                    }
+                    return next(action);
+                }
+            }
+        }
+    );
+
+    const webchatOptions = {
+        directLine: botConnection,
+        store: store,
+        styleOptions: styleOptions,
+        userID: user.id,
+        username: user.name,
+        locale: 'en'
+    };
+    startChat(user, webchatOptions);
+}
+
+function startChat(user, webchatOptions) {
+    const botContainer = document.getElementById('webchat');
+    window.WebChat.renderWebChat(webchatOptions, botContainer);
+}
